45e47f6a514445ba8fd8759f7d48f423e2a67885
Gabriela Yasmin <84574158+gabyyasmindesouza@users.noreply.github.com>
2023-05-30 11:54:09 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import './style.css'
+import Header from './componentes/Header'
+import Footer from './componentes/Footer'
+import Main from './componentes/Main'
+
+function App() {
+  
+  return (
+    <>
+    <body>
+      <div class="tela-inteira">
+      <Header/>
+      <Main/>
+      <Footer/>
+      </div>
+    </body>
+    </>
+  );
+}
+
+export default App;
